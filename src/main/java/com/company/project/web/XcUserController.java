7966fb036fd352331a98940531cb19309b24e8ae
cjm <1061076142@qq.com>
2020-11-18 22:39:18 +0800

    增加swagger2管理

--- src/main/java/com/company/project/web/XcUserController.java
+++ src/main/java/com/company/project/web/XcUserController.java
@@ -1,10 +1,13 @@
 package com.company.project.web;
+
 import com.company.project.core.Result;
 import com.company.project.core.ResultGenerator;
 import com.company.project.model.XcUser;
 import com.company.project.service.XcUserService;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,40 +16,46 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 import java.util.List;
 
-/**
-* Created by CodeGenerator on 2020/11/18.
-*/
 @RestController
 @RequestMapping("/xc/user")
+@Api(tags = {"/xc/user"},description="用户管理模块")
 public class XcUserController {
     @Resource
     private XcUserService xcUserService;
 
     @PostMapping("/add")
+    @ApiOperation(value = "新增用户", notes = "新增用户")
     public Result add(XcUser xcUser) {
         xcUserService.save(xcUser);
         return ResultGenerator.genSuccessResult();
     }
 
     @PostMapping("/delete")
-    public Result delete(@RequestParam Integer id) {
-        xcUserService.deleteById(id);
+    @ApiOperation(value = "逻辑删除用户", notes = "逻辑删除用户")
+    public Result delete(@RequestParam Long id) {
+        XcUser xcUser = new XcUser();
+        xcUser.setId(id);
+        xcUser.setIsDelete(true);
+        xcUserService.update(xcUser);
         return ResultGenerator.genSuccessResult();
     }
 
     @PostMapping("/update")
+    @ApiOperation(value = "修改用户", notes = "修改用户")
     public Result update(XcUser xcUser) {
         xcUserService.update(xcUser);
         return ResultGenerator.genSuccessResult();
     }
 
     @PostMapping("/detail")
+    @ApiOperation(value = "获取用户详情", notes = "获取用户详情")
     public Result detail(@RequestParam Integer id) {
         XcUser xcUser = xcUserService.findById(id);
         return ResultGenerator.genSuccessResult(xcUser);
     }
 
     @PostMapping("/list")
+    @ApiOperation(value = "获取用户详情", notes = "获取用户详情")
     public Result list(@RequestParam(defaultValue = "0") Integer page, @RequestParam(defaultValue = "0") Integer size) {
         PageHelper.startPage(page, size);
         List<XcUser> list = xcUserService.findAll();
