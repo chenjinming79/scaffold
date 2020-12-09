53a53101b3d35e57822b67854cba36660a2be3e8
cjm <1061076142@qq.com>
2020-12-09 14:53:01 +0800

    宠物详情

---
+++ src/main/java/com/company/project/web/XcPetController.java
@@ -0,0 +1,19 @@
+package com.company.project.web;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @ClassName XcPetController
+ * @Description //TODO
+ * @Author cjm
+ * @Date 2020/12/9 14:51
+ * @Version 1.0
+ **/
+@RequestMapping("/xc/pet")
+@Api(tags = {"/xc/pet"}, description = "星宠系统模块")
+public class XcPetController {
+
+
+
+}
